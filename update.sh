dc022cfd92d5338ddf35798e77f3731e1a1a7c6e
DarkXero-dev <steve@techxero.com>
2025-06-23 15:47:46 +0300

    Initial Commit

--- update.sh
+++
@@ -1,7 +0,0 @@
-sum="$(sha512sum ./mkinitcpio-firmware.hook | awk '{print $1}')"
-sed -i "s/sha512sums=(.*)/sha512sums=($sum)/g" ./PKGBUILD
-
-makepkg --printsrcinfo > .SRCINFO
-git add .
-git commit -m "updated"
-git push
